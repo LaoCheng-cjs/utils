485309050fd9e9822eaaa01d3a5cff3f98d9c07b
LaoCheng-cjs <49183028+LaoCheng-cjs@users.noreply.github.com>
2020-06-09 14:00:43 +0800

    新增脚本

--- src/index.js
+++ src/index.js
@@ -1,1 +1,308 @@
 
+/**
+ * 获取url（路由）参数，
+ * @param {url} 如果重复给参数，则和 vue-router 一样，都是以数组返回
+ * @default {url}
+ * @returns {Object}
+*/
+export const getUrlParam = urls => {
+    var url = urls || location.href
+    // 用JS拿到URL，如果函数接收了URL，那就用函数的参数。如果没传参，就使用当前页面的URL
+    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+    // 用来存储我们所有的参数
+    var obj = {};
+    // 如果没有传参，返回一个空对象
+    if (!queryString) {
+        return obj;
+    }
+    // stuff after # is not part of query string, so get rid of it
+    queryString = queryString.split('#')[0];
+    // 将参数分成数组
+    var arr = queryString.split('&');
+    for (var i = 0; i < arr.length; i++) {
+        // 分离成key:value的形式
+        var a = arr[i].split('=');
+        // 将undefined标记为true
+        var paramName = a[0];
+        var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+        if (paramName.match(/\[(\d+)?\]$/)) {
+            // 如果paramName不存在，则创建key
+            var key = paramName.replace(/\[(\d+)?\]/, '');
+            if (!obj[key]) obj[key] = [];
+            // 如果是索引数组 e.g. colors[2]
+            if (paramName.match(/\[\d+\]$/)) {
+                // 获取索引值并在对应的位置添加值
+                var index = /\[(\d+)\]/.exec(paramName)[1];
+                obj[key][index] = paramValue;
+            } else {
+                // 如果是其它的类型，也放到数组中
+                obj[key].push(paramValue);
+            }
+        } else {
+            // 处理字符串类型
+            if (!obj[paramName]) {
+                // 如果如果paramName不存在，则创建对象的属性
+                obj[paramName] = paramValue;
+            } else if (obj[paramName] && typeof obj[paramName] === 'string') {
+                // 如果属性存在，并且是个字符串，那么就转换为数组
+                obj[paramName] = [obj[paramName]];
+                obj[paramName].push(paramValue);
+            } else {
+                // 如果是其它的类型，还是往数组里丢
+                obj[paramName].push(paramValue);
+            }
+        }
+    }
+    return obj;
+}
+
+
+/**
+ * 去除空格：
+ * 参数一：字符串。
+ * 参数二可选：
+ *      1：默认去除所有空格
+ *      2：前后空格
+ *      3：前空格
+ *      4：后空格
+ * @param {string}
+ * @param {[type]}
+ * @returns {string}
+ * */
+export const trim = (str, type) => {
+    type = type || 1
+    switch (type) {
+        case 1:
+            return str.replace(/\s+/g, "");
+        case 2:
+            return str.replace(/(^\s*)|(\s*$)/g, "");
+        case 3:
+            return str.replace(/(^\s*)/g, "");
+        case 4:
+            return str.replace(/(\s*$)/g, "");
+        default:
+            return str;
+    }
+}
+
+
+/**
+ * 字母转大小写
+ *  type: 1: 首字母大写
+ *  2： 首字母小写
+ *  3： 大小写转换
+ *  4： 全部大写
+ *  5： 全部小写
+ * @param {string}
+ * @param {[type]}
+ * @returns {string}
+*/
+export const changeCase = (str, type) => {
+    type = type || 4
+    switch (type) {
+        case 1:
+            return str.replace(/\b\w+\b/g, function (word) {
+                return word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+            });
+        case 2:
+            return str.replace(/\b\w+\b/g, function (word) {
+                return word.substring(0, 1).toLowerCase() + word.substring(1).toUpperCase();
+            });
+        case 3:
+            return str.split('').map(function (word) {
+                if (/[a-z]/.test(word)) {
+                    return word.toUpperCase();
+                } else {
+                    return word.toLowerCase()
+                }
+            }).join('')
+        case 4:
+            return str.toUpperCase();
+        case 5:
+            return str.toLowerCase();
+        default:
+            return str;
+    }
+}
+
+
+
+/**
+ * 检测密码强度
+ * @param {string}
+ * @returns {number}
+*/
+export const checkPwd = (str) => {
+    var Lv = 0;
+    if (str.length < 6) {
+        return Lv
+    }
+    if (/[0-9]/.test(str)) {
+        Lv++
+    }
+    if (/[a-z]/.test(str)) {
+        Lv++
+    }
+    if (/[A-Z]/.test(str)) {
+        Lv++
+    }
+    if (/[\.|-|_]/.test(str)) {
+        Lv++
+    }
+    return Lv;
+}
+
+/**
+ * 将数字转换为大写金额
+ * @param {number}
+ * @returns {string}
+*/
+export const changeToChinese = (Num) => {
+    //判断如果传递进来的不是字符的话转换为字符
+    if (typeof Num == "number") {
+        Num = new String(Num);
+    };
+    Num = Num.replace(/,/g, "") //替换tomoney()中的“,”
+    Num = Num.replace(/ /g, "") //替换tomoney()中的空格
+    Num = Num.replace(/￥/g, "") //替换掉可能出现的￥字符
+    if (isNaN(Num)) { //验证输入的字符是否为数字
+        //alert("请检查小写金额是否正确");
+        return "";
+    };
+    //字符处理完毕后开始转换，采用前后两部分分别转换
+    var part = String(Num).split(".");
+    var newchar = "";
+    //小数点前进行转化
+    for (var i = part[0].length - 1; i >= 0; i--) {
+        if (part[0].length > 10) {
+            return "";
+            //若数量超过拾亿单位，提示
+        }
+        var tmpnewchar = ""
+        var perchar = part[0].charAt(i);
+        switch (perchar) {
+            case "0":
+                tmpnewchar = "零" + tmpnewchar;
+                break;
+            case "1":
+                tmpnewchar = "壹" + tmpnewchar;
+                break;
+            case "2":
+                tmpnewchar = "贰" + tmpnewchar;
+                break;
+            case "3":
+                tmpnewchar = "叁" + tmpnewchar;
+                break;
+            case "4":
+                tmpnewchar = "肆" + tmpnewchar;
+                break;
+            case "5":
+                tmpnewchar = "伍" + tmpnewchar;
+                break;
+            case "6":
+                tmpnewchar = "陆" + tmpnewchar;
+                break;
+            case "7":
+                tmpnewchar = "柒" + tmpnewchar;
+                break;
+            case "8":
+                tmpnewchar = "捌" + tmpnewchar;
+                break;
+            case "9":
+                tmpnewchar = "玖" + tmpnewchar;
+                break;
+        }
+        switch (part[0].length - i - 1) {
+            case 0:
+                tmpnewchar = tmpnewchar + "元";
+                break;
+            case 1:
+                if (perchar != 0) tmpnewchar = tmpnewchar + "拾";
+                break;
+            case 2:
+                if (perchar != 0) tmpnewchar = tmpnewchar + "佰";
+                break;
+            case 3:
+                if (perchar != 0) tmpnewchar = tmpnewchar + "仟";
+                break;
+            case 4:
+                tmpnewchar = tmpnewchar + "万";
+                break;
+            case 5:
+                if (perchar != 0) tmpnewchar = tmpnewchar + "拾";
+                break;
+            case 6:
+                if (perchar != 0) tmpnewchar = tmpnewchar + "佰";
+                break;
+            case 7:
+                if (perchar != 0) tmpnewchar = tmpnewchar + "仟";
+                break;
+            case 8:
+                tmpnewchar = tmpnewchar + "亿";
+                break;
+            case 9:
+                tmpnewchar = tmpnewchar + "拾";
+                break;
+        }
+        var newchar = tmpnewchar + newchar;
+    }
+    //小数点之后进行转化
+    if (Num.indexOf(".") != -1) {
+        if (part[1].length > 2) {
+            // alert("小数点之后只能保留两位,系统将自动截断");
+            part[1] = part[1].substr(0, 2)
+        }
+        for (i = 0; i < part[1].length; i++) {
+            tmpnewchar = ""
+            perchar = part[1].charAt(i)
+            switch (perchar) {
+                case "0":
+                    tmpnewchar = "零" + tmpnewchar;
+                    break;
+                case "1":
+                    tmpnewchar = "壹" + tmpnewchar;
+                    break;
+                case "2":
+                    tmpnewchar = "贰" + tmpnewchar;
+                    break;
+                case "3":
+                    tmpnewchar = "叁" + tmpnewchar;
+                    break;
+                case "4":
+                    tmpnewchar = "肆" + tmpnewchar;
+                    break;
+                case "5":
+                    tmpnewchar = "伍" + tmpnewchar;
+                    break;
+                case "6":
+                    tmpnewchar = "陆" + tmpnewchar;
+                    break;
+                case "7":
+                    tmpnewchar = "柒" + tmpnewchar;
+                    break;
+                case "8":
+                    tmpnewchar = "捌" + tmpnewchar;
+                    break;
+                case "9":
+                    tmpnewchar = "玖" + tmpnewchar;
+                    break;
+            }
+            if (i == 0) tmpnewchar = tmpnewchar + "角";
+            if (i == 1) tmpnewchar = tmpnewchar + "分";
+            newchar = newchar + tmpnewchar;
+        }
+    }
+    //替换所有无用汉字
+    while (newchar.search("零零") != -1)
+        newchar = newchar.replace("零零", "零");
+    newchar = newchar.replace("零亿", "亿");
+    newchar = newchar.replace("亿万", "亿");
+    newchar = newchar.replace("零万", "万");
+    newchar = newchar.replace("零元", "元");
+    newchar = newchar.replace("零角", "");
+    newchar = newchar.replace("零分", "");
+    if (newchar.charAt(newchar.length - 1) == "元") {
+        newchar = newchar + "整"
+    }
+    return newchar;
+}
